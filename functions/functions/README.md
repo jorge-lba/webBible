2173acfc0f45d2b3fd58b2de6841bdeda00182d1
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-12-08 23:41:38 -0300

    Reremovido mais alguns arquivos do antigo firebase; Configurada a hospedagem da função no firebase; add README para a função

---
+++ functions/functions/README.md
@@ -0,0 +1,18 @@
+# API para consulta á Biblía
+
+## Funcionalidade
+Está API foi criada para fezer consulta á Biblia de forma que retorne os valores até um unico vercículo.
+
+## Requisição
+
+### Parametros:
+
+__language__    - Selecionada o idioma (casos não seja informado retorna como padrão pr-bt).
+__version__     - Seleciona a versão (casos não seja informado retorna como padrão NVI).
+__book__        - Seleciona o livro (caso não seja informado retorna uma mensagem solicitando que preencha : caso seja invalido retorna os livros que existem).
+__chapter__     - Seleciona o capítulo (caso não seja informado retorna todos os capítulos do livro selecionado).
+__verse__       - Seleciona o versículo (caso não seja informado retorna o capítulo selecionado completo).
+
+### URL
+
+https://us-central1-webbibletest.cloudfunctions.net/bible?__language__=*pt-br*&__version__=*acf*&__book__=*mateus*&__chapter__=*1*&__verse__=*1*
---
+++ functions/functions/tddBibleEqual.js
@@ -0,0 +1,42 @@
+// Teste para comparar se a class está retornando o valor igual a o objeto
+
+const Bible = require('./controller');
+var assert = require( 'assert' );
+
+const tddBile = (language,version)=>{
+    bibleTDD = require(`./bible/${language}/${version}`)();
+    const bible = new Bible(language, version);
+
+    return [bibleTDD,bible];
+}
+
+const getTDD = (array, language = 'pt-br', version = 'NVI') =>{
+    const bible = `tddBile(\'${language}\',\'${version}\')[1]`;
+    const bibleTDD = `tddBile(\'${language}\',\'${version}\')[0]`;
+    let getClass = '.get(';
+    let getObject ='';
+
+    array.forEach(element => {
+        getClass += `\'${element}\',`;
+        getObject += `[\'${element}\']`;
+    });
+    getClass += ")";
+    return [eval(bible+getClass), eval(bibleTDD+getObject)];
+}
+
+const test = (array)=>{
+    array.forEach(e => {
+        const bibleClass = getTDD(e)[0][0]
+        const bibleObject = getTDD(e)[1]
+        
+        assert.equal(bibleClass,bibleObject,"Bible Class são diferentes Bible Object");
+
+    })
+
+    return 'Teste concluido com Sucesso!';
+}
+
+
+const arrayTests = [['genesis',1,1],['mateus',1,1], ['joao'],['judas',1,5]] 
+
+console.log(test(arrayTests))
