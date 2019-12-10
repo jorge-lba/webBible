8302a89ab1783acb6329a210ac0c5f15ba07067b
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2019-12-10 01:38:25 -0300

    Refatorando format_JSON

--- format_JSON/converter.js
+++ format_JSON/converter.js
@@ -1,29 +1,29 @@
-var fs = require('fs')
+var fs = require( 'fs' )
 
-const arrayJSON = fs.readdirSync(`${__dirname}/bible`)
+const arrayJSON = fs.readdirSync( `${__dirname}/bible` )
 
-const autoGetJSON = (array) =>{
+const autoGetJSON = ( array ) =>{
 
-    const result = array.map(e => {
+    const result = array.map(element => {
 
-        e = e.split('')
+        element = element.split('')
 
         let language = ''
         let version = ''
         let test = 'false'
 
-        e.forEach( e => {            
+        element.forEach( element => {            
 
-            ( e.charCodeAt ( 0 ) >= 65 && e.charCodeAt ( 0 ) <=90 )
+            ( element.charCodeAt ( 0 ) >= 65 && element.charCodeAt ( 0 ) <=90 )
                 ? test = 'true' 
-                : ( e == '.' )
+                : element == '.'
                     ? test = 'ponto'
                     : { }
 
             test == 'true'
-                ? version += e
+                ? version += element
                 : test == 'false'
-                    ? language += e
+                    ? language += element
                     : { }
 
         })
@@ -52,6 +52,22 @@ const assentRemove = ( text ) =>{
 
 };
 
+const array = ( value, obj ) => {
+    const fullArray = [ ];
+
+    for(let i = 0; i < value.length; i++){
+        const print2 = value[ i ].map(e => e)
+        fullArray[ i ] = print2;
+        obj[i+1] = {};
+        
+        print2.forEach((element, index) => {
+            obj[i+1][index+1] = element    
+        });
+
+    }
+    
+}
+
 for(let g = 0; g < arrayJSON.length; g++){
     
     const bLanguage = arrayLanguageVersion[g][0];
@@ -69,63 +85,40 @@ for(let g = 0; g < arrayJSON.length; g++){
     const getJSON = require(`${__dirname}/bible/${jsonFile}.json`);
 
     for(let a = 0; a < arrayWay.length; a++){
+
         way += `/${arrayWay[a]}`
         
-        if(!fs.existsSync(way)){
-            fs.mkdirSync(way)
-        }
+        if(!fs.existsSync(way)) fs.mkdirSync(way)
+
     }
 
-    for (let w =0; w < getJSON.length; w++){
+    for ( let w = 0 ; w < getJSON.length; w++ ){
         
-        let print = getJSON[w]
-        print = print.chapters.map(e => e)
+        let print = getJSON[ w ].chapters.map( e => e )
+        const obj = new Object;    
+        const book = assentRemove( getJSON[ w ].name )
 
-        const obj = new Object;
-        const name = getJSON[w].name
-        const book = assentRemove(name)
-
-        arrayBook.push(book);
+        arrayBook.push( book );
 
         obj.language = bLanguage
         obj.version = getJSON.verson
-        obj.title = name;
-        obj.abbreviation = assentRemove(getJSON[w].abbrev);
+        obj.title = getJSON[ w ].name;
+        obj.abbreviation = assentRemove( getJSON[ w ].abbrev );
 
-        if(w < 39){
-            obj.newTestament = false;
-        }else{
-            obj.newTestament = true;
+        w < 39
+            ? obj.newTestament = false
+            : obj.newTestament = true
 
-        }
+        array( print, obj )
 
+        jsCall.push( `${ book } = require( \`./_${ book }.json\` );`)
 
-        const array = value => {
-            const fullArray = [];
-
-            for(let i = 0; i < print.length; i++){
-                const print2 = value[i].map(e => e)
-                fullArray[i] = print2;
-                obj[i+1] = {};
-                
-                print2.forEach((element, index) => {
-                    obj[i+1][index+1] = element    
-                });
-
-            }
-            return obj;
-        }
-
-        array(print)
-
-        jsCall.push(`${book} = require(\`./_${book}.json\`);`)
-
-
-        fs.writeFile(`${way}/_${book}.json`,`${JSON.stringify(obj)}`, function(err) {
-            if(err) {
-                console.log(err);
+        fs.writeFile( `${ way }/_${ book }.json`, `${JSON.stringify( obj )}`, function( err ) {
+            if( err ) {
+                console.log( err );
             }
         });
+
     }
 
     fs.writeFile(`${way}/index.js`,`module.exports = () => {
@@ -147,5 +140,5 @@ for(let g = 0; g < arrayJSON.length; g++){
         }
     });
 
-    console.log(`Foi gerada a Biblía no idioma ${bLanguage} e versão ${bVersion}`);
+    console.log(`Foi gerada a Biblía no idioma ${ bLanguage } e versão ${bVersion}`);
 }
